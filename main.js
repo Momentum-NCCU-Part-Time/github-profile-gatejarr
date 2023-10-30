291e44a8ab843aa870f26307370aeb612a374450
Jarrod <gatejarr@gmail.com>
2023-10-29 20:24:51 -0400

    working on a second api fetch to populate repos section

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const gitAPI = "https://api.github.com/users/";
-let gitForm = document.getElementById("gitForm");
+const gitAPI = "https://api.github.com/users/gatejarr";
 let userProfile = document.getElementById("gitName");
 let button = document.getElementById("idButton");
 let results = document.getElementById("results");
@@ -7,46 +6,42 @@ let head = document.getElementById("head");
 let avatar = document.getElementById("imageContainer");
 let bio = document.getElementById("bio");
 let repos = document.getElementById("repos");
-let newSearch = false;
 
-gitForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let profile = userProfile.value;
+fetch(gitAPI)
+  .then((response) => {
+    return response.json();
+  })
+  .then((parsedJsonResponse) => {
+    console.log(parsedJsonResponse);
 
-  fetch(gitAPI + profile)
-    .then((response) => {
-      return response.json();
-    })
-    .then((parsedJsonResponse) => {
-      console.log(parsedJsonResponse);
+    let image = new Image();
+    image.src = parsedJsonResponse.avatar_url;
+    imageContainer.appendChild(image);
 
-      let image = new Image();
-      image.src = parsedJsonResponse.avatar_url;
-      imageContainer.appendChild(image);
+    let name = document.createElement("h1");
+    name.innerText = parsedJsonResponse.name;
+    head.appendChild(name);
 
-      let name = document.createElement("h2");
-      name.innerText = parsedJsonResponse.name;
-      head.appendChild(name);
+    let location = document.createElement("h4");
+    location.innerText = "Location: " + parsedJsonResponse.location;
+    bio.appendChild(location);
 
-      let location = document.createElement("h3");
-      location.innerText = "Location: " + parsedJsonResponse.location;
-      bio.appendChild(location);
+    let gitUrl = document.createElement("a");
+    gitUrl.innerText = "GitHub URL: " + parsedJsonResponse.html_url;
+    gitUrl.href = parsedJsonResponse.html_url;
+    bio.appendChild(gitUrl);
 
-      let gitUrl = document.createElement("a");
-      gitUrl.innerText = "GitHub URL: " + parsedJsonResponse.html_url;
-      gitUrl.href = parsedJsonResponse.html_url;
-      bio.appendChild(gitUrl);
+    let usrName = document.createElement("h4");
+    usrName.innerText = "GitHub username: " + parsedJsonResponse.login;
+    bio.appendChild(usrName);
 
-      let usrName = document.createElement("h3");
-      usrName.innerText = "GitHub username: " + parsedJsonResponse.login;
-      bio.appendChild(usrName);
+    //  let gitRepos = document.createElement("h4");
+    //  gitRepos.innerText = parsedJsonResponse.repos_url;
+    //  repos.appendChild(gitRepos);
 
-      let gitRepos = document.createElement("h4");
-      gitRepos.innerText = parsedJsonResponse.repos_url;
-      repos.appendChild(gitRepos);
+    results.appendChild(head);
+    results.appendChild(bio);
+    //  results.appendChild(repos);
 
-      results.appendChild(head);
-      results.appendChild(bio);
-      results.appendChild(repos);
-    });
-});
+    return fetch(parsedJsonResponse.repos_url);
+  });
